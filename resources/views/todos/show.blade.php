8dc9609d1a2ce189b7948da23eff57cec42437e5
Tomonori Ueno <t.uepon@gmail.com>
2018-08-22 00:11:22 +0900

    CRUD complete!

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -8,4 +8,8 @@
 	<p>{{$todo->body}}</p>
 	<br><br>
 	<a href="/todo/{{$todo->id}}/edit" class="btn btn-default">Edit</a>
+	{!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+		{{ Form::hidden('_method', 'DELETE')}}
+		{{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+	{!! Form::close() !!}
 @endsection
